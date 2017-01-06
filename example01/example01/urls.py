a5916d89588c4068da63188ebeba597bbbb419a1
seul lee <blacksangi14@naver.com>
2017-01-07 03:08:29 +0900

    no message

--- example01/example01/urls.py
+++ example01/example01/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from example01.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
