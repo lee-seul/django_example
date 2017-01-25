65a8a0b13b1a6be4435f77e284ad9a578487e99f
seul lee <blacksangi14@naver.com>
2017-01-26 01:12:33 +0900

    no message

--- example01/blog/urls.py
+++ example01/blog/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     
     url(r'^tag/$', TagTV.as_view(), name='tag_cloud'),
     url(r'^tag/(?P<tag>[^/]+(?u))/$', PostTOL.as_view(), name='tagged_object_list'),
+
+    url(r'^search/$', SearchFormView.as_view(), name='search'),    
 ]
