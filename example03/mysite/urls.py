57f7d75716bc7c97ea4d9fd793066aa13aa98b67
seul lee <blacksangi14@naver.com>
2017-03-06 23:04:48 +0900

    no message

--- example03/mysite/urls.py
+++ example03/mysite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
+        
     url(r'^admin/', admin.site.urls),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
