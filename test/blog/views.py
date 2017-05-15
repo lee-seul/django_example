556788c52ccc7da550a4c5027ba26f7caeba13c2
seul lee <blacksangi14@naver.com>
2017-05-16 00:14:38 +0900

    ch03

--- test/blog/views.py
+++ test/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    
+
+class PostDV(DetailView):
+    model = Post
+
+
