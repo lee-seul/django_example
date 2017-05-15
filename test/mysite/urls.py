d9a2eeaf41ae0c104190f68c5b443e229b7bc084
seul lee <blacksangi14@naver.com>
2017-05-16 00:37:25 +0900

    ch04

--- test/mysite/urls.py
+++ test/mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from mysite.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
+
     url(r'^admin/', admin.site.urls),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')), 
