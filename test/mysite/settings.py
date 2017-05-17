bf2736e42f0a03e603f5a34eab89bae0ed43d0a3
seul lee <blacksangi14@naver.com>
2017-05-17 18:55:43 +0900

    ch11

--- test/mysite/settings.py
+++ test/mysite/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# LOGIN_URL = '/accounts/login/'
+# LOGOUT_URL = '/accounts/logout/'
+LOGIN_REDIRECT_URL = '/'
+
+
--- test/mysite/urls.py
+++ test/mysite/urls.py
@@ -19,9 +19,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from mysite.views import HomeView
+from mysite.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/$', UserCreateView.as_view(), name='register'),
+    url(r'^accounts/register/done/$', UserCreateDoneTV.as_view(), 
+        name='register_done'),
 
     url(r'^admin/', admin.site.urls),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
